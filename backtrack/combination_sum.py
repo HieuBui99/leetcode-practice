47940d44f0cb6e85e75bb139b7f56c3b8d4013b2
HieuBui99 <hieu25121999@gmail.com>
2025-02-03 12:52:50 +0900

    combination sum 2

--- backtrack/combination_sum.py
+++ backtrack/combination_sum.py
@@ -17,3 +17,26 @@ def combination_sum(nums: List[int], target: int) -> List[List[int]]:
 
     backtrack(0, [])
     return res
+
+
+def combination_sum2(candidates: List[int], target: int) -> List[List[int]]:
+    res = []
+    candidates.sort()
+    def backtrack(i, combination):
+        cur_sum = sum(combination)
+        if cur_sum == target:
+            res.append(combination.copy())
+            return
+
+        if i >= len(candidates) or cur_sum > target:
+            return
+
+        combination.append(candidates[i])
+        backtrack(i+1, combination)
+        combination.pop()
+
+        while i + 1 < len(candidates) and candidates[i] == candidates[i+1]:
+            i += 1
+        backtrack(i+1, combination)
+
+    backtrack(0, [])
