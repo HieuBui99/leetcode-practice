24912eaed4b054c4ef848a96f10646215ac17ddd
HieuBui99 <hieu25121999@gmail.com>
2025-02-07 00:17:32 +0900

    subset2

--- backtrack/subset.py
+++ backtrack/subset.py
@@ -15,3 +15,21 @@ def subset(nums: List[int]) -> List[List[int]]:
 
     backtrack(0, [])
     return res
+
+
+def subset2(nums: List[int]) -> List[List[int]]:
+    res = []
+    nums.sort()
+    def backtrack(i, subset):
+        if i >= len(nums):
+            res.append(subset.copy())
+            return 
+        subset.append(nums[i])
+        backtrack(i+1, subset)
+        subset.pop()
+        while i + 1 < len(nums) and nums[i] == nums[i+1]:
+            i += 1
+        backtrack(i+1, subset)
+
+    backtrack(0, [])
+    return res
